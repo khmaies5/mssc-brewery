a014c3e15c48fa6d5f187259d2bdf4da83d52c05
Hassen Khmaies <khmaieshassen@hotmail.com>
2024-03-01 14:13:05 +0100

    initial commit

---
+++ src/test/java/com/khmaies/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khmaies.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
